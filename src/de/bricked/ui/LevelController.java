9a6f95dc35af7603739777c3ed2366cb7dcc59d7
deadlocker8 <deadlocker@gmx.de>
2016-10-20 00:17:36 +0200

    fixed ui bug

--- src/de/bricked/ui/LevelController.java
+++ src/de/bricked/ui/LevelController.java
@@ -13,7 +13,6 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.effect.BlendMode;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
