5b853706deea9d26e41ea6519b5d5c639165291b
Max Wittig <max.wittig95@gmail.com>
2016-10-20 15:03:46 +0200

    added LevelPackHandler for loading all levelpacks

--- src/de/bricked/game/levels/LevelPackHandler.java
+++ src/de/bricked/game/levels/LevelPackHandler.java
@@ -1,6 +1,30 @@
 package de.bricked.game.levels;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class LevelPackHandler
 {
-	//TODO
+    private static String directory = "src/de/bricked/resources/levelpacks/";
+	public static ArrayList<LevelPack> getAllLevelPacks()
+    {
+        ArrayList<LevelPack> levelPacks = new ArrayList<>();
+        for(File currentFile : getListOfFiles())
+        {
+            LevelPackReader levelPackReader = new LevelPackReader(currentFile.getName());
+            levelPacks.add(levelPackReader.read());
+        }
+        return levelPacks;
+    }
+
+    private static File[] getListOfFiles()
+    {
+        File folder = new File(directory);
+        return folder.listFiles();
+    }
+
+    public static void main(String[] args)
+    {
+        LevelPackHandler.getAllLevelPacks();
+    }
 }
