5818ad8855da990c3b9d4ab596972bf07cbb0852
Max Wittig <max.wittig95@gmail.com>
2016-10-23 11:20:12 +0200

    levelpacks are now sorted and default is always the first

--- src/de/bricked/game/levels/LevelPackHandler.java
+++ src/de/bricked/game/levels/LevelPackHandler.java
@@ -2,10 +2,41 @@ package de.bricked.game.levels;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class LevelPackHandler
 {
     private static String directory = "src/de/bricked/resources/levelpacks/";
+    private static final String DEFAULT_PACKAGE_NAME = "default";
+
+    private static ArrayList<LevelPack> sort(ArrayList<LevelPack> levelPackArrayList)
+    {
+        //sort level packs based on their name
+        levelPackArrayList.sort(new Comparator<LevelPack>()
+        {
+            @Override
+            public int compare(LevelPack o1, LevelPack o2)
+            {
+                return o1.getPackageName().toUpperCase().compareTo(o2.getPackageName().toUpperCase());
+            }
+        });
+
+        //bring default to front
+        LevelPack temp = levelPackArrayList.get(0);
+        for(int i=0; i < levelPackArrayList.size(); i++)
+        {
+            LevelPack currentPack = levelPackArrayList.get(i);
+            if(currentPack.getPackageName().equalsIgnoreCase(DEFAULT_PACKAGE_NAME))
+            {
+                //swap level packs
+                levelPackArrayList.set(0, currentPack);
+                levelPackArrayList.set(i, temp);
+                break;
+            }
+        }
+        return levelPackArrayList;
+    }
+
 	public static ArrayList<LevelPack> getAllLevelPacks()
     {
         ArrayList<LevelPack> levelPacks = new ArrayList<>();
@@ -14,7 +45,8 @@ public class LevelPackHandler
             LevelPackReader levelPackReader = new LevelPackReader(currentFile.getName());
             levelPacks.add(levelPackReader.read());
         }
-        return levelPacks;
+
+        return sort(levelPacks);
     }
 
     private static File[] getListOfFiles()
