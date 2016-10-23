a930885dc403041efb8303de4a43ad9c041375b1
Max Wittig <max.wittig95@gmail.com>
2016-10-23 11:31:44 +0200

    soundhandler volume

--- src/de/bricked/game/sound/SoundHandler.java
+++ src/de/bricked/game/sound/SoundHandler.java
@@ -10,18 +10,29 @@ import logger.Logger;
 
 public class SoundHandler
 {
-	public static void play(String soundID)
-	{		
-		try
-		{
-			String path = SoundHandler.class.getResource("/de/bricked/resources/sounds/" + soundID + ".mp3").toURI().toURL().toString();
-			Media sound = new Media(path);
-			MediaPlayer mediaPlayer = new MediaPlayer(sound);
-			mediaPlayer.setAutoPlay(true);				
-		}
-		catch(MalformedURLException | URISyntaxException e)
-		{
-			Logger.log(LogLevel.ERROR, Logger.exceptionToString(e));
-		} 
+    private double volume;
+
+    public SoundHandler()
+    {
+        volume = 0.0;
+    }
+
+    public void play(String soundID)
+	{
+        if(volume > 0)
+        {
+            try
+            {
+                String path = SoundHandler.class.getResource("/de/bricked/resources/sounds/" + soundID + ".mp3").toURI().toURL().toString();
+                Media sound = new Media(path);
+                MediaPlayer mediaPlayer = new MediaPlayer(sound);
+                mediaPlayer.setVolume(volume);
+                mediaPlayer.setAutoPlay(true);
+            }
+            catch (MalformedURLException | URISyntaxException e)
+            {
+                Logger.log(LogLevel.ERROR, Logger.exceptionToString(e));
+            }
+        }
 	}
 }
