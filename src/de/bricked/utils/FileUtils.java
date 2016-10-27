565333a74809d07f1a6a3424c09fa05bc5b7149c
Max Wittig <max.wittig95@gmail.com>
2016-10-27 18:55:23 +0200

    added method to add content from raw text url

--- src/de/bricked/utils/FileUtils.java
+++ src/de/bricked/utils/FileUtils.java
@@ -9,30 +9,51 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 
 public class FileUtils
 {
+
+    private static String getContentsFromInputStream(InputStream inputStream) throws Exception
+    {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream));
+        String line = "";
+        StringBuilder text = new StringBuilder();
+
+        while (line != null)
+        {
+            line = bufferedReader.readLine();
+            if (line != null)
+                text.append(line);
+        }
+        return text.toString();
+    }
+
     public static String getFileContentFromJar(String path)
     {
         try
         {
             InputStream inputStream = FileUtils.class.getResourceAsStream(path);
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream));
-            String line = "";
-            StringBuilder text = new StringBuilder();
-
-            while (line != null)
-            {
-                line = bufferedReader.readLine();
-                if (line != null)
-                    text.append(line);
-            }
-            return text.toString();
+            return getContentsFromInputStream(inputStream);
         }
         catch (Exception e)
         {
             Logger.log(LogLevel.ERROR, Logger.exceptionToString(e));
+        }
+        return null;
+    }
+
+    public static String getURLContentFromJar(String urlString)
+    {
+        try
+        {
+            InputStream in = new URL(urlString).openConnection().getInputStream();
+            return getContentsFromInputStream(in);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
         return null;
     }
