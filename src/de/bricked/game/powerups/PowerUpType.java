8198c03f8ec4c7fb21af50339aba27893062bf45
Max Wittig <max.wittig95@gmail.com>
2016-10-30 14:01:16 +0100

    added none powerup type

--- src/de/bricked/game/powerups/PowerUpType.java
+++ src/de/bricked/game/powerups/PowerUpType.java
@@ -3,14 +3,16 @@ package de.bricked.game.powerups;
 
 public enum PowerUpType
 {
+    NONE(0, -1),
     EXTRA_LIFE(1, -1),
     FASTER_BALL(2, 10),
     SLOWER_BALL(3, 10),
     EXPLODE_BALL(4, 10),
-    NO_COLLISION_BALL(5, 10);   
+    NO_COLLISION_BALL(5, 10);
 
     private int id;
     private int durationInSeconds;
+    public static PowerUpType[] types = PowerUpType.values();
 
     PowerUpType(int id, int durationInSeconds)
     {
@@ -23,8 +25,8 @@ public enum PowerUpType
         return id;
     }
 
-	public int getDurationInSeconds()
-	{
-		return durationInSeconds;
-	}   
+    public int getDurationInSeconds()
+    {
+        return durationInSeconds;
+    }
 }
