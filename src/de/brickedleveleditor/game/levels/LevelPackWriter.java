e9d30e04af93e37e81c6a05b67c42438f7f17a10
Max Wittig <max.wittig95@gmail.com>
2016-12-03 16:54:29 +0100

    Merge BrickedLevelEditor into Bricked

---
+++ src/de/brickedleveleditor/game/levels/LevelPackWriter.java
@@ -0,0 +1,52 @@
+package de.brickedleveleditor.game.levels;
+import de.bricked.game.bricks.Brick;
+import de.bricked.game.levels.Level;
+import de.bricked.game.paddle.PaddleSize;
+import de.bricked.game.powerups.PowerUp;
+
+
+import java.util.ArrayList;
+
+public class LevelPackWriter
+{
+    private ArrayList<Level> levels;
+
+    public LevelPackWriter()
+    {
+        levels = new ArrayList<>();
+    }
+
+    public void writePack()
+    {
+
+    }
+
+    private String getBoard(ArrayList<Brick> bricks)
+    {
+        StringBuilder builder = new StringBuilder();
+        for(Brick brick : bricks)
+        {
+            String brickName = brick.getType().getID();
+            String powerupName = "";
+            PowerUp powerUp = brick.getPowerUp();
+            if(powerUp == null)
+            {
+                powerupName = "0";
+            }
+            else
+            {
+                powerupName = String.valueOf(powerUp.getID());
+            }
+            builder.append(brickName + powerupName + " ");
+        }
+        builder.delete(builder.length()-1,builder.length());
+        System.out.println(builder.toString());
+        return builder.toString();
+    }
+
+    public void addLevel(String levelname, String author, int position, int difficulty, int startLives, PaddleSize paddleSize, ArrayList<Brick> bricks)
+    {
+        Level level = new Level(levelname, author, position, difficulty, startLives, paddleSize, getBoard(bricks));
+        levels.add(level);
+    }
+}
---
+++ src/de/brickedleveleditor/ui/BrickLabel.java
@@ -0,0 +1,52 @@
+package de.brickedleveleditor.ui;
+
+
+import de.bricked.game.bricks.BrickType;
+import de.bricked.game.powerups.PowerUpType;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+
+public class BrickLabel extends Label
+{
+    private BrickType brickType;
+    private PowerUpType powerUpType;
+
+    public BrickLabel()
+    {
+    }
+
+    public BrickLabel(BrickType brickType, PowerUpType powerUpType)
+    {
+        super(brickType.getID() + powerUpType.getId());
+        this.brickType = brickType;
+        this.powerUpType = powerUpType;
+
+    }
+
+    public BrickLabel(String text, Node graphic)
+    {
+        super(text, graphic);
+    }
+
+    public BrickType getBrickType()
+    {
+        return brickType;
+    }
+
+    public void setBrickType(BrickType brickType)
+    {
+        this.brickType = brickType;
+        setText(this.brickType.getID()+this.powerUpType.getId());
+    }
+
+    public PowerUpType getPowerUpType()
+    {
+        return powerUpType;
+    }
+
+    public void setPowerUpType(PowerUpType powerUpType)
+    {
+        this.powerUpType = powerUpType;
+        setText(this.brickType.getID()+this.powerUpType.getId());
+    }
+}
